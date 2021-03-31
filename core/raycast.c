18f7cf6b1eb91fa9253f4422f1d950c9fae521f1
MurkyMeow <dinosowermurky@gmail.com>
2021-03-31 17:25:29 +0900

    fix warnings

--- core/raycast.c
+++ core/raycast.c
@@ -49,17 +49,17 @@ float get_wall_distance(const map_t* map, const player_t* player, float angle) {
   float ray_dir_x = cosf(angle);
   float ray_dir_y = sinf(angle);
 
-  int map_x = (int)player->x;
-  int map_y = (int)player->y;
+  float map_x = floorf(player->x);
+  float map_y = floorf(player->y);
 
-  int map_step_x = ray_dir_x < 0 ? -1 : 1;
-  int map_step_y = ray_dir_y < 0 ? -1 : 1;
+  float map_step_x = ray_dir_x < 0 ? -1 : 1;
+  float map_step_y = ray_dir_y < 0 ? -1 : 1;
 
   float side_step_x = fabsf(1 / ray_dir_x);
   float side_step_y = fabsf(1 / ray_dir_y);
 
-  float side_dist_x = map_step_x * (map_x + map_step_x * 0.5 + 0.5 - player->x) * side_step_x;
-  float side_dist_y = map_step_y * (map_y + map_step_y * 0.5 + 0.5 - player->y) * side_step_y;
+  float side_dist_x = map_step_x * (map_x + map_step_x * 0.5F + 0.5F - player->x) * side_step_x;
+  float side_dist_y = map_step_y * (map_y + map_step_y * 0.5F + 0.5F - player->y) * side_step_y;
 
   int side = -1;
 
@@ -74,12 +74,12 @@ float get_wall_distance(const map_t* map, const player_t* player, float angle) {
       side = 1;
     }
 
-    int map_index = map_x + map->w * map_y;
+    int map_index = (int)map_x + map->w * (int)map_y;
     int map_data = map->values[map_index];
 
     if (map_data > 0) {
-      float hit_dist = side == 0 ? (map_x - player->x + 0.5 - map_step_x * 0.5) / ray_dir_x
-                                 : (map_y - player->y + 0.5 - map_step_y * 0.5) / ray_dir_y;
+      float hit_dist = side == 0 ? (map_x - player->x + 0.5F - map_step_x * 0.5F) / ray_dir_x
+                                 : (map_y - player->y + 0.5F - map_step_y * 0.5F) / ray_dir_y;
       return hit_dist;
     }
   }
